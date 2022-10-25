78b0f8b04a4e13e042e8159a8e3b2bad3278edff
daniellimae <Daniel.Silva.ext@cyrela.com.br>
2022-10-24 21:51:08 -0300

    feat: wellcome page

---
+++ screens/Wellcome/wellcome.page.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+import * as Styles from "./wellcome.styles";
+
+export default function WellcomePage() {
+  return <Styles.Container></Styles.Container>;
+}
